5e1d05442092e7ba4924c82cfe98c4e11d054774
Michael Hutchison <m.hutchison@student.unsw.edu.au>
2017-08-10 11:29:09 +1000

    refactored code for clarity

--- beacon_locator/scripts/locator.py
+++ beacon_locator/scripts/locator.py
@@ -12,7 +12,7 @@ import itertools
 import numpy as np
 
 
-class reader:
+class locator:
 
 	def __init__(self):
 		self.rgbSubscriber = rospy.Subscriber('/camera/rgb/image_color', Image, self.rgbCallback) # subscribe to images from the Kinect
@@ -62,26 +62,27 @@ class reader:
 
 		# create a black image for visualising detected rectangles
 		# note: only leave uncommented for testing purposes
-		output = np.zeros((480,640,3), np.uint8)
+#		output = np.zeros((480,640,3), np.uint8)
 
 		# draw visualisation of possible beacon colour candidates
 		# note: only leave uncommented for testing purposes
-		for rect in rects:
-			cv2.rectangle(output, (rect[1], rect[2]), (rect[3], rect[4]), self.colours[rect[0]]["colour"], 3)
+#		for rect in rects:
+#			cv2.rectangle(output, (rect[1], rect[2]), (rect[3], rect[4]), self.colours[rect[0]]["colour"], 3)
 
 		print "Frame Processed"
 		# iterate through all pairs of detected rectangles
 		for rect1, rect2 in itertools.permutations(rects, 2):
 			# if the rect1 is positioned directly on top of rect2
-			if rect1[6] < rect2[6] and (((rect1[1] >= rect2[1] and rect1[1] <= rect2[3]) or (rect1[3] >= rect2[1] and rect1[3] <= rect2[3])) or ((rect2[1] >= rect1[1] and rect2[1] <= rect1[3]) or (rect2[3] >= rect1[1] and rect2[3] <= rect1[3]))) and rect1[4] - rect2[2] < 20:
+			if rect1[6] < rect2[6] and self.doesOverlap(rect1[1],rect1[3],rect2[1],rect2[3]) and rect1[4] - rect2[2] < 20:
+				# check if beacon is a valid colour combination
 				for b in self.beacons:
 					if b["top"] == rect1[0] and b["bottom"] == rect2[0]:
-						print "Beacon %d: %s / %s" % (b["id"], rect1[0], rect2[0])
+						print "Beacon %d: %s / %s" % (b["id"], rect1[0], rect2[0]) # beacon has successfully identified
 
 		# display the visualisation
 		# note: only leave uncommented for testing purposes
-		cv2.imshow("images", output)
-		cv2.waitKey(0)
+#		cv2.imshow("images", output)
+#		cv2.waitKey(0)
 
 
 	# returns a list of rectangles the are possible candidates [colour, minX, minY, maxX, maxY, centerX, centerY]
@@ -109,10 +110,13 @@ class reader:
 				rects.append([colour, x, y, x+w, y+h, x+w/2, y+h/2]) # add rect to list of candidate colour rectangles
 
 		return rects # return any colour rectangles
-		
+	
+	# returns boolean indicating if two provided ranges overlap
+	def doesOverlap(self, r1_low, r1_high, r2_low, r2_high):
+		return ((r1_low >= r2_low and r1_low <= r2_high) or (r1_high >= r2_low and r1_high <= r2_high)) or ((r2_low >= r1_low and r2_low <= r1_high) or (r2_high >= r1_low and r2_high <= r1_high))
 
 if __name__ == '__main__':
-	rd = reader()
+	rd = locator()
 	rospy.init_node('beacon_locator_node') # create node
 	rospy.spin()
 	
