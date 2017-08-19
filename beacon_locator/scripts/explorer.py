b40b2a448fa5df6a02282cbda06ee5f16d98fd6d
nunodasneves <ndasneves@gmail.com>
2017-08-19 04:58:39 -0700

    added some code for down-sampling the map; runs quite slow in python

--- beacon_locator/scripts/explorer.py
+++ beacon_locator/scripts/explorer.py
@@ -5,7 +5,7 @@
     Subscribes to a String on the '/beacon_locator_node/status' topic
     Publishes a PoseStamped to the '/move_base/simple_goal' topic
     First records the initial position of the rover (when this node starts)
-    Downsamples occupancy grid to roughly robot-sized squares
+    Turns the occupancy grid into a costmap-like matrix so that it can better evaluate frontiers
     Finds closest unobstructed frontier using breadth first search and sends the robot there
     Once all the beacons are found, it sends the robot back to its initial position
     By: Nuno Das Neves
@@ -30,8 +30,12 @@ class Explorer():
         self.tfListener = tf.TransformListener()
 
         self.goalPub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+
         self.mapSub = rospy.Subscriber('/map', OccupancyGrid, self.gotMap)
         self.statusSub = rospy.Subscriber('/beacon_locator_node/status', String, self.gotStatus)
+
+        # only needed for testing
+        self.mapPub = rospy.Publisher('/explore_map', OccupancyGrid, queue_size=5) 
     
     def gotStatus(self, status):
 
@@ -40,11 +44,44 @@ class Explorer():
             self.returnHome = True
     
     def gotMap(self, grid):
+
         if self.done or self.returnHome:
             rospy.loginfo("Done with exploring")
             return
         
+        MAX_GOAL_DIST = 1
+        MIN_GOAL_DIST = 0.2
+
+        RES_FACTOR = 5
+
+        OG_THRESHOLD = 20
         
+        # we need to 'expand' the impassable areas of the map so that we don't try to go through walls if there's a tiny gap
+        rospy.loginfo("Map: \n%s" % str(grid.info))
+        gWidth = grid.info.width
+        gHeight = grid.info.height
+
+        newHeight = gHeight/RES_FACTOR
+        newWidth = gWidth/RES_FACTOR
+        newVals = [[0]*newHeight for i in range(newWidth)]
+        for i in range(gHeight*gWidth):
+            if grid.data[i] > OG_THRESHOLD:
+                newVals[(i/gHeight)/RES_FACTOR][(i%gWidth)/RES_FACTOR] = 100
+
+        newData = [0]*gWidth*gHeight
+        for i in range(gWidth*gHeight):
+            newData[i] = newVals[(i/gHeight)/RES_FACTOR][(i%gWidth)/RES_FACTOR]
+        grid.data = tuple(newData)
+
+        rospy.loginfo("Published new map")
+
+        self.mapPub.publish(grid)
+
+        # search for the closest frontier using bfs. If it's too close, find another one
+
+        # set a pose at some reasonable distance; 
+        # basically go through the points on the path until we reach the max distance we want to set a point at
+
 
     def explore(self):
 
@@ -80,7 +117,7 @@ class Explorer():
                 self.done = True
                 return
 
-            rospy.sleep(1)
+            rospy.sleep(2)
 
         
 
