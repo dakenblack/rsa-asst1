f6abaa17bb5408b383fa4b03056b7fe938f4ead8
Michael Hutchison <z5061367@unsw.edu.au>
2017-08-29 16:08:40 +1000

    updates timestamps of markers each time marker is detected

--- beacon_locator/scripts/locator.py
+++ beacon_locator/scripts/locator.py
@@ -193,6 +193,7 @@ class locator:
 		marker = beacon["marker"]
 		marker.pose.position.x = beaconPos["x"]
 		marker.pose.position.y = beaconPos["y"]
+		marker.header.stamp = rospy.Time.now();
 
 		if beacon["found"] == 0: # if this is the first time the beacon has been found, create the marker and flag it as found
 			marker.id = beacon["id"]
@@ -204,7 +205,6 @@ class locator:
 			marker.scale.z = 1.0
 			marker.pose.orientation.w = 1.0
 			marker.pose.position.z = 0
-			marker.header.stamp = rospy.Time.now();
 
 			# compute the required colours
 			baseCol = ColorRGBA(0.0, 0.0, 0.0, 1.0)
