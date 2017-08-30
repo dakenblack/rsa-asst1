b3efbfa3f44abe1d26972cc0a49844ebebb416e9
Jabez Wilson <jabez.wilson0@gmail.com>
2017-08-30 10:19:26 +1000

    stuff we used for final demo

--- beacon_locator/scripts/locator.py
+++ beacon_locator/scripts/locator.py
@@ -193,14 +193,15 @@ class locator:
 		marker = beacon["marker"]
 		marker.pose.position.x = beaconPos["x"]
 		marker.pose.position.y = beaconPos["y"]
+                marker.header.stamp = rospy.Time.now()
 
 		if beacon["found"] == 0: # if this is the first time the beacon has been found, create the marker and flag it as found
 			marker.id = beacon["id"]
 			marker.header.frame_id = "/comp3431/map"
 			marker.type = 11
 			marker.action = 0
-			marker.scale.x = 1.0
 			marker.scale.y = 1.0
+                        marker.scale.x = 1.0
 			marker.scale.z = 1.0
 			marker.pose.orientation.w = 1.0
 			marker.pose.position.z = 0
